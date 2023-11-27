d365413fdcc76b26da46aaff25d35c0d02b32bd9
tmb2k01 <marcsello.ma@gmail.com>
2023-11-27 19:21:13 +0100

    Input Output process

--- AI/main.py
+++ AI/main.py
@@ -3,22 +3,51 @@ import cv2
 import car_recognition
 import plate_recognition
 import transform
+import argparse
+import os
 
-def main():
-    IMAGE_URL = './dataset/21555931.jpg'
+CarRecognizer = car_recognition.CarRecognition()
+PlateRecognizer = plate_recognition.PlateRecognition()
+#OCR = orc.OCR() - inicializálni az OCR-t
+
+def get_path(directory, file):
+    return directory + '/' + file
+
+def get_result_file(filename):
+    base_name = filename.split('.')[0]
+    return base_name + ".txt"
+
+def process_image(IMAGE_URL):
     image = cv2.imread(IMAGE_URL)
 
-    CarRecognizer = car_recognition.CarRecognition()
     car_image = CarRecognizer.getCar(image)
 
-    PlateRecognizer = plate_recognition.PlateRecognition()
     plate_image = PlateRecognizer.getPlate(car_image)
 
     transformed_image = transform.transformImage(plate_image)
 
-    cv2.imshow("Car detection", transformed_image)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    # result = getString_OCR(transformed_image) - meghívni az OCR-t
+    # if result == "":
+    #   result = getString_OCR(plate_image) - ha nem sikerül a transform, próba az eredetivel
+    # return result
+    return "xoxo" # törölni
+
+def main(input_dir, output_dir):
+    for image_src in os.listdir(input_dir):
+        result = process_image(get_path(input_dir, image_src))
+        with open(get_path(output_dir, get_result_file(image_src)), 'w') as file:
+            file.write(result)
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Process some paths.")
+
+    # Adding arguments with default values
+    parser.add_argument("--input", help="Input directory path", default="./dataset/source")
+    parser.add_argument("--output", help="Output directory path", default="./dataset/done")
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Call main function with parsed arguments
+    main(args.input, args.output)
